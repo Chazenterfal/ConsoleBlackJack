6249a1595f5193de2f5c5782606560c34eecca92
Nikithn <54394238+Chazenterfal@users.noreply.github.com>
2020-07-22 12:36:42 +0300

    First Steps
    Some code.

---
+++ ConsoleBlackJack/ConsoleBlackJack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleBlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Blackjack - Welcome! Write /'s/' for start. (Any other letter for quit)");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+            if (s == "s")
+            {
+                Console.WriteLine("Nice Work");
+            }
+            else
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
+    }
+}
