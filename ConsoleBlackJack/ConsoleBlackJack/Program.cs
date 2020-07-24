f0633f71ef335ed048aba0311a2b806e0f2dd6cc
Nikithn <54394238+Chazenterfal@users.noreply.github.com>
2020-07-24 12:09:56 +0300

    Codefix Update
    Some improvements, thanks for @anivire, happy birthday yeah, sempai!

--- ConsoleBlackJack/ConsoleBlackJack/Program.cs
+++ ConsoleBlackJack/ConsoleBlackJack/Program.cs
@@ -6,63 +6,58 @@ namespace ConsoleBlackJack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Simple Blackjack - Welcome! Write /'s/' for start. (Any other letter for quit)");
+            Console.WriteLine("Simple Blackjack - Welcome! Write 's' for start. (Any other key for quit)");
             string s = Console.ReadLine();
             int raz = 0;
-            int a = 0;
+            int EXIT = 0;
             int sum1 = 0;
+            Random num = new Random();
             for (int i = 1; i<3; i++)
             {
-                Random num = new Random();
-                int brosok1 = num.Next(2,11);
-                sum1 = sum1 + brosok1;
-                Console.WriteLine("Card Value - ");
-                Console.WriteLine(brosok1);
+                int Card1 = num.Next(2,11);
+                sum1 = sum1 + Card1;
+                Console.WriteLine($"Card Value - {Card1}");
             }
 
-            while (a != 1)
+            while (EXIT != 1)
             {
                 Console.WriteLine("Write S for cards, else write Y.");
                 string S = Console.ReadLine();
-                if (S == "Y")
+                if (S == "S")
                 {
-                    Random num = new Random();
-                    int brosok1 = num.Next(2, 11);
-                    sum1 = sum1 + brosok1;
-                    Console.WriteLine("Card Value - ");
-                    Console.WriteLine(brosok1);
+                    int Card1 = num.Next(2, 11);
+                    sum1 = sum1 + Card1;
+                    Console.WriteLine($"Card Value - {Card1}");
                 }
                 else
                 {
-                    a = 1;
+                    EXIT = 1;
                 }
             }
             int sum2 = 0;
             for (int i = 1; i<3; i++)
                 {
-                    Random num = new Random();
-                    int brosok1 = num.Next(2, 11);  
-                    sum2 = sum2 + brosok1;
+                    int Card1 = num.Next(2, 11);  
+                    sum2 = sum2 + Card1;
                 }
 
             while (raz != 0)
             {
                 raz -= 1;
-                Random num = new Random();
-                int brosok1 = num.Next(2, 11);
-                sum2 = sum2 + brosok1;
+                int Card1 = num.Next(2, 11);
+                sum2 = sum2 + Card1;
             }
             if (sum1 > sum2 && sum1<= 21)
             {
-                Console.WriteLine("You won!");
+                Console.WriteLine($"The banker have {sum2} and it means you won!");
             }
             else if (sum1 == sum2)
             {
-                Console.WriteLine("The banker won!");
+                Console.WriteLine($"The banker have {sum2} and it means you lose!");
             }
             else
             {
-                Console.WriteLine("The banker won!");
+                Console.WriteLine($"The banker have {sum2} and it means you lose!");
             }
         
         }
